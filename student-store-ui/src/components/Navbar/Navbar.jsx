e9061859e3819a4747cd873506ae2ed4b6df2d94
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-06-12 11:58:58 -0700

    navbar setup

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import * as React from "react"
 import "./Navbar.css"
+import {  Link } from "react-router-dom";
+import logo from './logo.svg';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <div className="items">
+        <div className="logo">
+          <a href="/"><img src={logo}/></a>
+        </div>
+        <ul className="links">
+          <a href="/" className="i-link"><li>Home</li></a>
+          <a href="/#products" className="i-link"><li>Products</li></a>
+          <a href="about" className="i-link"><li>About Us</li></a>
+          <a href="contact" className="i-link"><li>Contact Us</li></a>
+        </ul>
+      </div>
     </nav>
   )
 }
